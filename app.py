9abfbd8ad5d3b569532e84f995ca34370b6ffd5c
renisalcedo <salcedoreynaldo5@gmail.com>
2018-11-16 21:24:08 -0500

    Instantiated book and added initialization under main

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from db.db import connectdb
+from src.controllers.Book import Book
 
-# Will initialize the database Connection
-db = connectdb()
-db_cursor = db.cursor()
+if __name__ == '__main__':
+    # Will initialize the database Connection
+    db = connectdb()
+    db_cursor = db.cursor()
 
-# Column Names
-db_props = (db_table, "title", "author", "description")
+    # Column Names
+    db_props = (db_table, "title", "author", "description")
 
-db_cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(db_name))
-db_cursor.execute("USE {}".format(db_name))
-db_cursor.execute("CREATE TABLE IF NOT EXISTS {0[0]} \
-                    (id INT AUTO_INCREMENT PRIMARY KEY, \
-                    {0[1]} VARCHAR(255) NOT NULL, {0[2]} VARCHAR(255) NOT NULL, \
-                    {0[3]} VARCHAR(255) NOT NULL)".format(db_props)
-                 )
+    db_cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(db_name))
+    db_cursor.execute("USE {}".format(db_name))
+    db_cursor.execute("CREATE TABLE IF NOT EXISTS {0[0]} \
+                        (id INT AUTO_INCREMENT PRIMARY KEY, \
+                        {0[1]} VARCHAR(255) NOT NULL, {0[2]} VARCHAR(255) NOT NULL, \
+                        {0[3]} VARCHAR(255) NOT NULL)".format(db_props)
+                    )
+
+    # instance of book
+    book = Book(db)
