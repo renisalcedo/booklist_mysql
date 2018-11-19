24630738ce67589bdb2a3eec1c26302ba8f25deb
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-11-19 18:26:13 -0500

    Modified get selection and added different selections

--- src/interface.py
+++ src/interface.py
@@ -1,17 +1,20 @@
 class Interface:
-    def __init__(self):
-        self.select = None
+    def __init__(self, db_table):
+        self.db_table = db_table
+        self.selections = {'1': self.create_book, '2': self.find_book,
+                           '3': self.update_book, '4': self.delete_book,
+                           '5': self.quit
+                          }
 
     def greet(self):
+        """ Greets the user with a welcoming message """
         print("------------------------------")
         print("Welcome To Book List!")
         print("------------------------------")
         print('\n')
 
-    def set_selection(self, select):
-        self.select = select
-
     def menu(self):
+        """ Display a menu for the user to choose from """
         self.greet()
 
         print("1) New Book")
@@ -19,3 +22,13 @@ class Interface:
         print("3) Update Book")
         print("4) Delete Book")
         print("5) Quit")
+
+    def get_selection(self):
+        """ Gets the selection from the user """
+        select = input("Make Selection: ")
+        self.selections[select]()
+
+    def main(self):
+        """ Main function that runs the interface logic """
+        self.menu()
+        self.get_selection()
